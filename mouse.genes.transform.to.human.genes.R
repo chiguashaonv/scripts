13744d1e160fd6ad101f62da526575dc69b72146
chiguashaonv <863546066@qq.com>
2018-07-01 18:12:05 +0800

    Update mouse.genes.transform.to.human.genes.R

--- mouse.genes.transform.to.human.genes.R
+++ mouse.genes.transform.to.human.genes.R
@@ -1,4 +1,8 @@
+human = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+mouse = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
+
 mouse.to.human = getLDS(attributes = c("ensembl_gene_id"),
 	filters = "ensembl_gene_id", values = x , mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=T)
 human.to.mouse = getLDS(attributes = c("hgnc_symbol"),
 	filters = "hgnc_symbol", values = x , mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+
